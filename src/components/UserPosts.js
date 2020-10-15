309284b8c60ba42c2234c8fdae9b89b62b790462
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-15 12:00:46 -0400

    refactored userPosts to use existing fetch

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -3,43 +3,39 @@ import { useParams } from 'react-router-dom'
 import fetchJSON from '../modules/fetch.service.js'
 import './UserPosts.css'
 
-function UserPosts() {
-    // First set up some state varibles with the useState to then use the hooks api to fetch data
-    const [userPosts, setUserPosts] = useState()
-    const [error, setErrors] = useState()
-
-    //now lets use params and get the userId from the url
+function UserPosts({ posts: userPosts, error }) {
+    //first lets get the userId from the url
     const { userId } = useParams()
-    // Now using our FetchJSON function and the useEFffect function from React we can fetch
-    // The JSON data for the photos and set either that or an error (if failed)
-    useEffect(() => {
-        fetchJSON({ route: 'posts', query: { userId: userId } })
-            .then(_posts => setUserPosts(_posts))
-            .catch(err => setErrors(err))
-    }, [userId])
 
     // Now check if we have comments and errors and return the appropriate type
-    if (!userPosts) return null
+    if (userPosts) {
+        //first filter out the posts once we confirm we have some
+        const filteredPosts = userPosts.filter(post => post.userId === parseInt(userId))
+
+        //if we get here we have comments and no errors so lets map them to a JSX element and return that componenet
+        const postElements = filteredPosts.map(post =>
+            <li key={post.id}>
+                <h5>{post.title}</h5>
+                <article>{post.body}</article>
+            </li>
+        )
+
+        return (
+            <div className="page">
+                <div className="UserPosts">
+                    <h1>Users Posts</h1>
+                    <ul>
+                        {postElements}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
     if (error) return (<div>{error.message}</div>)
 
-    //if we get here we have comments and no errors so lets map them to a JSX element and return that componenet
-    const postElements = userPosts.map(post =>
-        <li key={post.id}>
-            <h5>{post.title}</h5>
-            <article>{post.body}</article>
-        </li>
-    )
 
-    return (
-        <div className="page">
-            <div className="UserPosts">
-                <h1>Users Posts</h1>
-                <ul>
-                    {postElements}
-                </ul>
-            </div>
-        </div>
-    )
+    //if we reach this point return null
+    return null
 }
 
 export default UserPosts
