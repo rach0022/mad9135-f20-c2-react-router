261ec2ab09e7174919a7294cba8a87c8b1f3142f
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-21 11:04:08 -0400

    added gh-pages to repo now to deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import { HashRouter as Router, Switch, Route } from 'react-router-dom' //uncomment for deployment
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom' //uncomment for deployment
 // import logo from './logo.svg';
 import './App.css';
 //import modules
