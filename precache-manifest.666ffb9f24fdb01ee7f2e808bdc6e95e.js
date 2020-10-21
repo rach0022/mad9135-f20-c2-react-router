59e79d9e552ac1a60c3d6b971af78e44039b480f
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-21 11:04:39 -0400

    Updates

---
+++ precache-manifest.666ffb9f24fdb01ee7f2e808bdc6e95e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e18121b26a0dabbec255962f559c82",
+    "url": "/mad9135-f20-c2-react-router/index.html"
+  },
+  {
+    "revision": "327d2ddea15e2f622ba5",
+    "url": "/mad9135-f20-c2-react-router/static/css/main.12f738f1.chunk.css"
+  },
+  {
+    "revision": "853c9f8f9fe99ddc283b",
+    "url": "/mad9135-f20-c2-react-router/static/js/2.9ad0ac55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mad9135-f20-c2-react-router/static/js/2.9ad0ac55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327d2ddea15e2f622ba5",
+    "url": "/mad9135-f20-c2-react-router/static/js/main.abe8f5da.chunk.js"
+  },
+  {
+    "revision": "54545d0f67049b88330d",
+    "url": "/mad9135-f20-c2-react-router/static/js/runtime-main.4fe883f2.js"
+  },
+  {
+    "revision": "e699f3e12687a9376d38123e6fa7d776",
+    "url": "/mad9135-f20-c2-react-router/static/media/album.e699f3e1.png"
+  },
+  {
+    "revision": "a3df0b7adb5de0e2a36abd46c42b7208",
+    "url": "/mad9135-f20-c2-react-router/static/media/brand.a3df0b7a.png"
+  },
+  {
+    "revision": "d7e0aee52700817918c52cfc787573e7",
+    "url": "/mad9135-f20-c2-react-router/static/media/users.d7e0aee5.png"
+  }
+]);
